57d79574b460f8668e2d3ebaf160f83995e833e7
App Generator <support@appseed.us>
2021-06-22 21:32:04 +0300

    App - Added new model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):                                 # <- NEW
+    title            = models.CharField(max_length=100)   # <- NEW 
+    author           = models.CharField(max_length=100)   # <- NEW
+    publication_date = models.DateField()                 # <- NEW
